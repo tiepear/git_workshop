fbcbb930ff41504a40dff129eff87d6c4b0d6d02
Kaito Okura <e175748@ie.u-ryukyu.ac.jp>
2018-11-22 18:45:14 +0900

    try git

--- trygit/ViewController.swift
+++ trygit/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        label.text = "trygit"
     }
 
 
