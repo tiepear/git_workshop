ff4ecee924e675839fddc157e25fd2a73e9dd43e
Sudate <e175754@ie.u-ryukyu.ac.jp>
2018-11-22 13:16:04 +0900

    sample first

---
+++ sample/sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sample
+//
+//  Created by kentarou sudate on 2018/11/22.
+//  Copyright © 2018 kentarou sudate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
